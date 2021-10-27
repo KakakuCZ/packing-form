e35732a70c584e18207ccc07a709e0f9be0faf71
Michael Kufner <>
2021-10-27 10:52:21 +0200

    Add config with data to API
    This should not be commited and it should be solved locally (like ENV variables or local config files). For this usage this is absolutely ok.

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    packingApi: {
+        username: "Kakaku",
+        apiKey: "fc89ba32f800a9ce1791d804b5be08ce",
+        url: "https://eu.api.3dbinpacking.com/packer/pack"
+    },
+}
